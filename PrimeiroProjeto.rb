c8b516b34553391aa3863545b4dbb855142cb113
Julio Carvalho <juliocarvalho@Julios-MacBook-Pro.local>
2023-02-16 11:52:15 -0300

    Desafio concluido

---
+++ PrimeiroProjeto.rb
@@ -0,0 +1,13 @@
+print "Digite seu Nome: "
+
+nome = gets.chomp
+
+print "Digiete seu Sobrenome: "
+
+sobrenome = gets.chomp
+
+print "Digite sua idade: "
+
+idade = gets.chomp
+
+puts "oi #{nome} #{sobrenome}, idade:#{idade}"
